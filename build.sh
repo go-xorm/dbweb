588256b05ec8a513729850619aa8c45077bd27d9
Lunny Xiao <xiaolunwen@gmail.com>
2015-04-06 22:56:15 +0800

    add langs

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ mkdir ./dbweb
 GOOS=linux GOARCH=amd64 go build -o ./dbweb/dbweb
 cp -r ./static ./dbweb/
 cp -r ./templates ./dbweb/
+cp -r ./langs ./dbweb/
 cp ./*.pem ./dbweb/
 tar zcvf ./dbweb.tar.gz ./dbweb
 echo "done."
